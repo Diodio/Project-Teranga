7014c24b0cece2131ebd4ab3c1caf49e8fb277eb
diodio <diodio.mbodj@kiwi.sn>
2015-12-26 19:13:03 +0000

    pdf

--- app/pdf/stockContenuPdf.php
+++ app/pdf/stockContenuPdf.php
@@ -8,14 +8,12 @@ $password = $paramsConnexion['password'];
 $connexion = mysqli_connect($hostname, $username, $password) or trigger_error(mysqli_error(), E_USER_ERROR);
 mysqli_set_charset($connexion, "utf8");
 mysqli_select_db($connexion, $database);
-$sql = "SELECT * FROM stock_final sf ,usine u WHERE sf.codeUsine=u.code AND u.code=" . $usineCode;
+$sql = "SELECT * FROM stock_reel sr ,usine u WHERE sr.codeUsine=u.code AND u.code='.$usineCode.'";
 $Result = mysqli_query($connexion, $sql) or die(mysqli_error($connexion));
 $row = mysqli_fetch_array($Result);
-//Cette requete permet de recuperer les produits d'un achat
-$sqlProduit="SELECT p.libelle designation,al.prixUnitaire prixUnitaire,al.quantite quantite,al.montant montant FROM achat a, ligne_achat al, produit p WHERE a.id=al.achat_id AND al.produit_id=p.id AND a.id=" . $achatId;
-// $sqlProduit="SELECT p.libelle designation,al.prixUnitaire prixUnitaire,al.quantite quantite,al.montant montant FROM achat a, ligne_achat al, produit p WHERE a.id=al.achat_id AND al.produit_id=p.id AND a.id=" . $achatId;
+//Cette requete permet de recuperer les produits d'une usine
+$sqlProduit="SELECT p.libelle designation, sr.stock stock FROM produit p, usine u, stock_reel sr WHERE p.id=sr.produit_id AND u.code='.$usineCode.'";
 $ResultProduit = mysqli_query($connexion, $sqlProduit) or die(mysqli_error($connexion));
-$ResultProduit1 = mysqli_query($connexion, $sqlProduit) or die(mysqli_error($connexion));
 ?>
 
 <style type="text/css">
@@ -50,25 +48,8 @@ td    { vertical-align: top; }
     <table cellspacing="0" style="width: 100%; text-align: left;font-size: 10pt">
         <tr>
             <td style="width:40%;"></td>
-            <td style="width:30%; "><span  style="font-size: 25px;" >BON D'ACHAT</span></td>
+            <td style="width:30%; "><span  style="font-size: 25px;" >Stock Réel de <?php echo $row['nomUsine']; ?></span></td>
             <td >Numero: <?php echo $row['numero']; ?></td>
-        </tr>
-    </table>
-    <table cellspacing="0" style="color:#444444";width: 100%; text-align: center; font-size: 14px">
-        <tr>
-            <td >
-                Mareyeur: <?php echo $row['nom']; ?>
-            </td>
-        </tr>
-        <tr>
-        <td >
-                Origine: <?php echo $row['adresse']; ?>
-        </td>
-        </tr>
-        <tr>
-        <td >
-                Heure de reception: <?php echo $row['heureReception']; ?>
-        </td>
         </tr>
     </table>
     
@@ -76,10 +57,8 @@ td    { vertical-align: top; }
     <br>
     <table cellspacing="0" style="width: 100%; border: solid 1px black; background: #E7E7E7; text-align: left; font-size: 10pt;">
         <tr>
-            <th style="width: 42%">Désignation</th>
-            <th style="width: 18%;">Prix Unitaire</th>
-            <th style="width: 15%; text-align: right;">Quantité</th>
-            <th style="width: 25%;text-align: right;">Montant</th>
+            <th style="width: 80%">Désignation</th>
+            <th style="width: 20%;">Stock Réel</th>
         </tr>
     </table>
     
@@ -88,16 +67,14 @@ td    { vertical-align: top; }
     $totalPrix=0;
     $totalQuantite=0;
    while ($rowProduit = mysqli_fetch_array($ResultProduit)) {
-       $total =$total+ $rowProduit['montant'];
-       $totalPrix =$totalPrix+ $rowProduit['prixUnitaire'];
-       $totalQuantite =$totalQuantite+ $rowProduit['quantite'];
+//        $total =$total+ $rowProduit['montant'];
+//        $totalPrix =$totalPrix+ $rowProduit['prixUnitaire'];
+       $totalQuantite =$totalQuantite+ $rowProduit['stock'];
 ?>
     <table cellspacing="0" style="width: 100%; border: solid 1px black; background: #F7F7F7; text-align: left; font-size: 10pt;">
         <tr>
-            <td style="width: 42%; text-align: left"><?php echo $rowProduit['designation']; ?></td>
-            <td style="width: 18%; text-align: left"><?php echo $rowProduit['prixUnitaire']; ?> </td>
-            <td style="width: 15%; text-align: right"><?php echo $rowProduit['quantite']; ?></td>
-            <td style="width: 25%; text-align: right;"><?php echo $rowProduit['montant']; ?> </td>
+            <td style="width: 80%; text-align: left"><?php echo $rowProduit['designation']; ?></td>
+            <td style="width: 20%; text-align: left"><?php echo $rowProduit['stock']; ?> </td>
         </tr>
     </table>
 <?php
@@ -105,10 +82,8 @@ td    { vertical-align: top; }
 ?>
     <table cellspacing="0" style="width: 100%; border: solid 1px black; background: #E7E7E7; text-align: center; font-size: 10pt;">
         <tr>
-            <th style="width: 18%; text-align: left;">Total : </th>
-            <th style="width: 31%; text-align: right;"><?php echo number_format($totalPrix, 2, ',', ' '); ?> </th>
-            <th style="width: 26%; text-align: right;"><?php echo number_format($totalQuantite, 0, ',', ' '); ?>kg </th>
-            <th style="width: 25%; text-align: right;"><?php echo number_format($total, 2, ',', ' '); ?> </th>
+            <th style="width: 80%; text-align: left;">Total : </th>
+            <th style="width: 20%; text-align: left;"><?php echo number_format($totalQuantite, 0, ',', ' '); ?>kg </th>
         </tr>
     </table>
     <br>
@@ -124,103 +99,5 @@ td    { vertical-align: top; }
             </tr>
         </table>
     </nobreak>
-    
-    <br/><br/><br/>
-    <bookmark title="Lettre" level="0" ></bookmark>
-    <table cellspacing="0" style="width: 100%; text-align: center; font-size: 14px">
-        <tr>
-            <td style="width: 40%; ">
-                <span style="font-size: 20px;color:blue" >MACFISH</span>
-                <br>
-                 <span style="font-size: 20px;color:blue" >PRODUCTION SUARL</span>
-                 <br><br>
-                <span >TEL : 338218470 / 338363512</span>
-            </td>
-            <td style="width: 40%;">
-                <span style="margin-left:30px;"></span>
-            </td>
-            <td style="width: 25%; color: #444444;">
-                Dakar, le <?php echo date("d-m-Y");  ;?>
-            </td>
-        </tr>
-    </table>
-    <br>
-    <br>
-    <table cellspacing="0" style="width: 100%; text-align: left;font-size: 10pt">
-        <tr>
-            <td style="width:40%;"></td>
-            <td style="width:30%; "><span  style="font-size: 25px;" >BON D'ACHAT</span></td>
-            <td >Numero: <?php echo $row['numero']; ?></td>
-        </tr>
-    </table>
-    <table cellspacing="0" style="color:#444444";width: 100%; text-align: center; font-size: 14px">
-        <tr>
-            <td >
-                Mareyeur: <?php echo $row['nom']; ?>
-            </td>
-        </tr>
-        <tr>
-        <td >
-                Origine: <?php echo $row['adresse']; ?>
-        </td>
-        </tr>
-        <tr>
-        <td >
-                Heure de reception: <?php echo $row['heureReception']; ?>
-        </td>
-        </tr>
-    </table>
-    
-    <br>
-    <br>
-    <table cellspacing="0" style="width: 100%; border: solid 1px black; background: #E7E7E7; text-align: left; font-size: 10pt;">
-        <tr>
-            <th style="width: 42%">Désignation</th>
-            <th style="width: 18%;">Prix Unitaire</th>
-            <th style="width: 15%; text-align: right;">Quantité</th>
-            <th style="width: 25%;text-align: right;">Montant</th>
-        </tr>
-    </table>
-    
-<?php
-    $total1 =0;
-    $totalPrix1=0;
-    $totalQuantite1=0;
-   while ($rowProduit1 = mysqli_fetch_array($ResultProduit1)) {
-       $total1 =$total1+ $rowProduit1['montant'];
-       $totalPrix1 =$totalPrix1+ $rowProduit1['prixUnitaire'];
-       $totalQuantite1 =$totalQuantite1+ $rowProduit1['quantite'];
-?>
-    <table cellspacing="0" style="width: 100%; border: solid 1px black; background: #F7F7F7; text-align: left; font-size: 10pt;">
-        <tr>
-            <td style="width: 42%; text-align: left"><?php echo $rowProduit1['designation']; ?></td>
-            <td style="width: 18%; text-align: left"><?php echo $rowProduit1['prixUnitaire']; ?> </td>
-            <td style="width: 15%; text-align: right"><?php echo $rowProduit1['quantite']; ?></td>
-            <td style="width: 25%; text-align: right;"><?php echo $rowProduit1['montant']; ?> </td>
-        </tr>
-    </table>
-<?php
-    }
-?>
-    <table cellspacing="0" style="width: 100%; border: solid 1px black; background: #E7E7E7; text-align: center; font-size: 10pt;">
-        <tr>
-            <th style="width: 18%; text-align: left;">Total : </th>
-            <th style="width: 31%; text-align: right;"><?php echo number_format($totalPrix1, 2, ',', ' '); ?> </th>
-            <th style="width: 26%; text-align: right;"><?php echo number_format($totalQuantite1, 0, ',', ' '); ?>kg </th>
-            <th style="width: 25%; text-align: right;"><?php echo number_format($total, 2, ',', ' '); ?> </th>
-        </tr>
-    </table>
-    <br>
-    
-    <nobreak>
-        <br>
-        <table cellspacing="0" style="width: 100%; text-align: left;">
-            <tr>
-                <td style="width:25%;">Le Comptable</td>
-                <td style="width:25%"></td>
-                <td style="width:38%"></td>
-                <td style="width:25%"> Le Mareyeur</td>
-            </tr>
-        </table>
-    </nobreak>
+  
 </page>
