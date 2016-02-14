c1b307f704722a5f5dd59e42060c960ce040f483
diodio <diodio.mbodj@kiwi.sn>
2016-02-14 20:47:31 +0000

    inventaire achat

--- app/Inventaire/achatInventaire.php
+++ app/Inventaire/achatInventaire.php
@@ -164,184 +164,14 @@
                 $("#tot"+index).val("0");
             }
        }
-         $(document).delegate('#tab_logic tr td', 'click', function (event) {
-        var id = $(this).closest('tr').attr('id');
-        var counter = id.slice(-1);
        
-      $( "#qte"+counter ).keyup(function() {
-            calculPoids(counter);
-         });
-        
-    });
-    
-           
-        $( "#stockReel" ).keyup(function() {
-            verifiePoidsReel();
-         });
          
-         function verifiePoidsReel(index){
-           var stockProvisoire = parseFloat($("#stockProvisoire").val());
-           var stockReel=parseFloat($("#stockReel").val());
-         
-           if(stockReel>=stockProvisoire) {
-                    $.gritter.add({
-                        title: 'Notification',
-                        text: 'Le stock réel défini doit etre inférieur au stock provisoire',
-                        class_name: 'gritter-error gritter-light'
-                    }); 
-                   $("#stockReel").val("0"); 
-               }
-       }
-            checkedAchatsContains = function(item) {
-                for (var i = 0; i < checkedAchats.length; i++) {
-                    if (checkedAchats[i] == item)
-                        return true;
-                }
-                return false;
-            };
             // Persist checked Message when navigating
             
-            
-            persistChecked = function() {
-                $('input[type="checkbox"]', "#LIST_ACHATS_INVENTAIRES").each(function() {
-                    if (checkedAchatsContains($(this).val())) {
-                        $(this).attr('checked', 'checked');
-                    } else {
-                        $(this).removeAttr('checked');
-                    }
-                });
-            };
-             $('table th input:checkbox').on('click', function() {
-                var that = this;
-                $(this).closest('table').find('tr > td:first-child input:checkbox').each(function() {
-                    this.checked = that.checked;
-                    if (this.checked)
-                    {
-                        checkedAchatsAdd($(this).val());
-                      //  MessageSelected();
-                        $('#TAB_GROUP a[href="#TAB_INFO"]').tab('show');
-			$('#TAB_MSG_VIEW').hide();
-                        nbTotalAchatsChecked=checkedAchats.length;
-                    }
-                    else
-                    {
-                        checkedAchatsRemove($(this).val());
-                   //    MessageUnSelected();
-                        $('#TAB_GROUP a[href="#TAB_INFO"]').tab('show');
-			$('#TAB_MSG_VIEW').hide();
-                    }
-                    $(this).closest('tr').toggleClass('selected');
-                });
-            });
-            
-             $('#LIST_ACHATS_INVENTAIRES tbody').on('click', 'input[type="checkbox"]', function() {
-                context=$(this);
-                if ($(this).is(':checked') && $(this).val() != '*') {
-                    checkedAchatsAdd($(this).val());
-                    MessageSelected();
-                } else {
-                    checkedAchatsRemove($(this).val());
-                    MessageUnSelected();
-                }
-                ;
-                if(!context.is(':checked')){
-                    $('table th input:checkbox').removeAttr('checked');
-                }else{
-                    if(checkedAchats.length==nbTotalAchatsChecked){
-                        $('table th input:checkbox').prop('checked', true);
-                    }
-                }
-            });
-            
-         
-            $('#SAVE').attr("disabled", true);
-            MessageSelected = function(click)
-            {
-                if (checkedAchats.length == 1){
-                    $('#SAVE').attr("disabled", false);
-                    loadAchatsSelected(checkedAchats[0]);
-                    $('#TAB_MSG_VIEW').show();
-		    $('#TAB_GROUP a[href="#TAB_MSG"]').tab('show');
-                }else
-                {
-                    $('#SAVE').attr("disabled", true);
-                    $('#nomProduit').text("");
-                    $('#stockProvisoire').val("");
-                    $('#stockReel').val("");
-                    $('#nombreCarton').val("");
-                    $('#nombreParCarton').val("");
-                    
-                    $('#TAB_GROUP a[href="#TAB_INFO"]').tab('show');
-                    $('#TAB_MSG_VIEW').hide();
-                    
-                }
-                if(checkedAchats.length==nbTotalAchatsChecked){
-                    $('table th input:checkbox').prop('checked', true);
-                }
-            };
-            MessageUnSelected = function()
-            {
-               if (checkedAchats.length === 1){
-                   $('#SAVE').attr("disabled", false);
-                    loadAchatsSelected(checkedAchats[0]);
-		    $('#TAB_MSG_VIEW').show();
-                    $('#TAB_GROUP a[href="#TAB_MSG"]').tab('show');
-                }
-                else
-                {
-                    $('#SAVE').attr("disabled", true);
-                    $('#nomProduit').text("");
-                    $('#stockProvisoire').val("");
-                    $('#stockReel').val("");
-                    $('#nombreCarton').val("");
-                    $('#nombreParCarton').val("");
-                    $('#SAVE').attr("disabled", false);
-                    
-                    $('#TAB_GROUP a[href="#TAB_INFO"]').tab('show');
-                    $('#TAB_MSG_VIEW').hide();
-                    $("#BTN_MSG_GROUP").popover('destroy');
-                    $("#BTN_MSG_CONTENT").popover('destroy');
-                }
-                $('table th input:checkbox').removeAttr('checked');
-            };
-
-            // Add checked item to the array
-            checkedAchatsAdd = function(item) {
-                if (!checkedMessageContains(item)) {
-                    checkedAchats.push(item);
-                }
-            };
-            // Remove unchecked items from the array
-            checkedAchatsRemove = function(item) {
-                var i = 0;
-                while (i < checkedAchats.length) {
-                    if (checkedAchats[i] == item) {
-                        checkedAchats.splice(i, 1);
-                    } else {
-                        i++;
-                    }
-                }
-            };
-            checkedMessageContains = function(item) {
-                for (var i = 0; i < checkedAchats.length; i++) {
-                    if (checkedAchats[i] == item)
-                        return true;
-                }
-                return false;
-            };
-            showPopover = function(idButton, colis){
-            $("#" + idButton).popover({
-                html: true,
-                trigger: 'focus',
-                placement: 'left',
-                title: '<i class="icon-group icon-"></i> Détail colis ',
-                content: colis
-            }).popover('toggle');
-         };
              loadAchats = function(dateDebut, dateFin, regle) {
                 nbTotalAchatsChecked = 0;
                 checkedAchats = new Array();
-                var url =  '<?php echo App::getBoPath(); ?>/produit/AchatController.php';
+                var url =  '<?php echo App::getBoPath(); ?>/achat/AchatController.php';
 
                 if (oTableAchats != null)
                     oTableAchats.fnDestroy();
@@ -358,14 +188,6 @@
                     },
                     "aoColumnDefs": [
                         {
-//                             "aTargets": [0],
-//                             "bSortable": false,
-//                             "fnCreatedCell": function(nTd, sData, oData, iRow, iCol) {
-//                                 $(nTd).css('text-align', 'center');
-//                             },
-//                             "mRender": function(data, type, full) {
-//                                 return '<label><input type="checkbox" id="' + data + '" value="' + data + '"><span class="lbl"></span></label>';
-//                             }
                         },
                         {
                         "aTargets": [4],
@@ -377,9 +199,6 @@
                             action=$('<div></div>');
                             action.addClass('hidden-phone pull-right visible-desktop action-buttons');
                             
-                            btnGrps.tooltip({
-                                title: 'Consulter Détail des colis'
-                            });
                             $(nTd).append(action);
                            
                         }
@@ -421,11 +240,13 @@
                         aoData.push({"name": "offset", "value": "1"});
                         aoData.push({"name": "rowCount", "value": "10"});
                         userProfil=$.cookie('profil');
+                        
                         if(userProfil==='admin'){
                             aoData.push({"name": "codeUsine", "value": "*"});
                         }
                         else
                         aoData.push({"name": "codeUsine", "value": "<?php echo $codeUsine;?>"});
+                        
                         aoData.push({"name": "dateDebut", "value": dateDebut});
                         aoData.push({"name": "dateFin", "value": dateFin});
                         aoData.push({"name": "regle", "value": regle});
@@ -453,214 +274,8 @@
                 });
             };
             
-            loadAchats();
-            loadAchatsSelected = function(produitId)
-            {
-                 var url;
-                 url = '<?php echo App::getBoPath(); ?>/produit/ProduitController.php';
+            loadAchats($('dateDebut').val(),$('dateFin').val(), $('regle').val());
 
-                $.post(url, {produitId: produitId, ACTION: "<?php echo App::ACTION_VIEW_DETAILS; ?>"}, function(data) {
-                  data = $.parseJSON(data);
-                 // data = data[0];
-                    $('#nomProduit').text(data.designation);
-                    $('#stockProvisoire').val(data.stockProvisoire);
-                    //$('#stockReel').val(data.stockReel);
-                    
-               }).error(function(error) { });
-            };
-
-            $("#MNU_VALIDATION").click(function()
-            {
-                if (checkedAchats.length == 0)
-                    bootbox.alert("Veuillez selectionnez un achat");
-                else if (checkedAchats.length >= 1)
-                {
-                     bootbox.confirm("Voulez vous vraiment valider cet achat", function(result) {
-                    if(result){
-                    var produitId = checkedAchats[0];
-                    $.post("<?php echo App::getBoPath(); ?>/achat/AchatsController.php", {produitId: produitId, ACTION: "<?php echo App::ACTION_ACTIVER; ?>"}, function(data)
-                    {
-                        if (data.rc == 0)
-                        {
-                            bootbox.alert("Achats(s) validé(s)");
-                        }
-                        else
-                        {
-                            bootbox.alert(data.error);
-                        }
-                    }, "json");
-                    $("#MAIN_CONTENT").load("<?php echo App::getHome(); ?>/app/achat/achatListe.php", function () {
-                        });
-                         }
-                    });
-                }
-            });
-
-
-            
-            $("#MNU_ANNULATION").click(function()
-            {
-                if (checkedAchats.length == 0)
-                    bootbox.alert("Veuillez selectionnez un achat");
-                else if (checkedAchats.length >= 1)
-                {
-                     bootbox.confirm("Voulez vous vraiment annuler cet achat", function(result) {
-                    if(result){
-                    var produitId = checkedAchats[0];
-                    $.post("<?php echo App::getBoPath(); ?>/achat/AchatsController.php", {produitId: produitId, ACTION: "<?php echo App::ACTION_DESACTIVER; ?>"}, function(data)
-                    {
-                        if (data.rc === 0)
-                        {
-                            bootbox.alert("Achats(s) annulés(s)");
-                            
-                        }
-                        else
-                        {
-                            bootbox.alert(data.error);
-                        }
-                    }, "json");
-                    $("#MAIN_CONTENT").load("<?php echo App::getHome(); ?>/app/achat/achatListe.php", function () {
-                        });
-                         }
-                    });
-                }
-            });
-            
-        DemoulageProcess = function ()
-        {
-            
-           $('#SAVE').attr("disabled", true);
-            var ACTION = '<?php echo App::ACTION_INSERT; ?>';
-            
-            var stockReel= $('#stockReel').val();
-            var codeUsine = "<?php echo $codeUsine ?>";
-            var login = "<?php echo $login ?>";
-            var $table = $("table");
-            rows = [],
-            header = [];
-
-        //$table.find("thead th").each(function () {
-        //    header.push($(this).html().trim());
-        //});
-        header = ["#","nbCarton","qte","total"];
-        $table.find("tbody tr").each(function () {
-            var row = {};
-
-            $(this).find("td").each(function (i) {
-                var key = header[i];
-                var value;
-                    valueSelect = $(this).find('select').val();
-                    valueInput = $(this).find('input').val();
-                if (typeof valueInput !== "undefined")
-                    value=valueInput;
-                if (typeof valueSelect !== "undefined")
-                    value=valueSelect;
-                row[key] = value;
-            });
-
-            rows.push(row);
-        });
-    
-    
-            var tbl=JSON.stringify(rows);
-            var formData = new FormData();
-            formData.append('ACTION', ACTION);
-            formData.append('produitId', checkedAchats[0]);
-            formData.append('stockReel', stockReel);
-            formData.append('jsonCarton', tbl);
-            formData.append('codeUsine', codeUsine);
-            formData.append('login', login);
-            $.ajax({
-                url: '<?php echo App::getBoPath(); ?>/demoulage/DemoulageController.php',
-                type: 'POST',
-                processData: false,
-                contentType: false,
-                dataType: 'JSON',
-                data: formData,
-                success: function (data)
-                {
-                    if (data.rc == 0)
-                    {
-                        $.gritter.add({
-                            title: 'Notification',
-                            text: data.action,
-                            class_name: 'gritter-success gritter-light'
-                        });
-                        loadAchats();
-                        $('#nombreCarton').val("");
-                        $('#nombreParCarton').val("");
-                    } 
-                    else
-                    {
-                        $.gritter.add({
-                            title: 'Notification',
-                            text: data.error,
-                            class_name: 'gritter-error gritter-light'
-                        });
-                        
-                        $('#SAVE').attr("disabled", false);
-                    };
-                    
-                },
-                error: function () {
-                    alert("failure - controller");
-                    $('#SAVE').attr("disabled", false);
-                }
-            });
-
-        };
-        $("#SAVE").bind("click", function () {
-        $('#validation-form').validate({
-			errorElement: 'div',
-			errorClass: 'help-block',
-			focusInvalid: false,
-			ignore: "",
-			rules: {
-				stockReel: {
-                                    required: true
-				},
-				nombreParCarton: {
-                                    required: true
-				},
-				nombreCarton: {
-                                    required: true
-				}
-				
-			},
-	
-			messages: {
-				stockReel: {
-					required: "Champ obligatoire."
-				},
-				nombreParCarton: {
-					required: "Champ obligatoire."
-				},
-				nombreCarton: {
-					required: "Champ obligatoire."
-				}
-			},
-	
-	
-			highlight: function (e) {
-				$(e).closest('.form-group').removeClass('has-info').addClass('has-error');
-			},
-	
-			success: function (e) {
-				$(e).closest('.form-group').removeClass('has-error');//.addClass('has-info');
-				$(e).remove();
-			},
-	
-			errorPlacement: function (error, element) {
-				 error.insertAfter(element);
-			},
-	
-			submitHandler: function (form) {
-				DemoulageProcess();
-			},
-			invalidHandler: function (form) {
-			}
-		});
-        });
 
         $("#MNU_IMPRIMER").click(function()
         {
