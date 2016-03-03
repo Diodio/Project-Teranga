b9f42aac94f54b574fba108229038f1c2b6c3279
matgere <matar.gueye@kiwi.sn>
2016-03-03 10:15:20 +0000

    Merge origin/master
    Conflicts:
    	app/achat/achatListeMagasinier.php

---
+++ backend/src/be/LigneColisBonSortie.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace BonSortie;
+
+/** @Entity @HasLifecycleCallbacks
+ * @Table(name="ligne_colis_bonsortie") * */
+class LigneColisBonSortie {
+
+    /** @Id
+     * @Column(type="integer"), @GeneratedValue
+     */
+    protected $id;
+    
+     /**
+     * @Column(type="integer", nullable=false)
+     * */
+    protected $nombreCarton;
+    
+    /**
+     * @Column(type="decimal", scale=2, precision=10, nullable=true)
+     * */
+    protected $quantiteParCarton;
+
+     /**
+     * @Column(type="integer", nullable=false)
+     * */
+    protected $produit_id;
+
+    
+    
+
+    /** @Column(type="datetime", nullable=true) */
+    protected $createdDate;
+
+    /** @Column(type="datetime", nullable=true) */
+    protected $updatedDate;
+
+    /** @Column(type="datetime", nullable=true) */
+    protected $deletedDate;
+
+    function getId() {
+        return $this->id;
+    }
+
+    function getNombreCarton() {
+        return $this->nombreCarton;
+    }
+
+    function getQuantiteParCarton() {
+        return $this->quantiteParCarton;
+    }
+
+    function getProduit_id() {
+        return $this->produit_id;
+    }
+
+    function getCreatedDate() {
+        return $this->createdDate;
+    }
+
+    function getUpdatedDate() {
+        return $this->updatedDate;
+    }
+
+    function getDeletedDate() {
+        return $this->deletedDate;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setNombreCarton($nombreCarton) {
+        $this->nombreCarton = $nombreCarton;
+    }
+
+    function setQuantiteParCarton($quantiteParCarton) {
+        $this->quantiteParCarton = $quantiteParCarton;
+    }
+
+    function setProduit_id($produit_id) {
+        $this->produit_id = $produit_id;
+    }
+
+    function setCreatedDate($createdDate) {
+        $this->createdDate = $createdDate;
+    }
+
+    function setUpdatedDate($updatedDate) {
+        $this->updatedDate = $updatedDate;
+    }
+
+    function setDeletedDate($deletedDate) {
+        $this->deletedDate = $deletedDate;
+    }
+
+    
+    
+        
+        /** @PrePersist */
+    public function doPrePersist() {
+        $this->createdDate = new \DateTime("now");
+        $this->updatedDate = new \DateTime("now");
+    }
+
+}
