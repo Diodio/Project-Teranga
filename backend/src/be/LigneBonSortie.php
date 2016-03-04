5362889f7408038b5437f55193611183868cdaa6
matgere <matar.gueye@kiwi.sn>
2016-03-04 22:53:46 +0000

    Merge origin/master

--- backend/src/be/LigneBonSortie.php
+++ backend/src/be/LigneBonSortie.php
@@ -12,6 +12,11 @@ class LigneBonSortie {
     protected $id;
     
     /**
+     * @Column(type="integer", nullable=false)
+     * */
+    protected $nombreCarton;
+    
+    /**
      * @Column(type="decimal", scale=2, precision=10, nullable=true)
      * */
     protected $quantite;
