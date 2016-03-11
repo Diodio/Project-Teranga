46ca7472d1b67f688dfa9a2e52e519fa48764335
matgere <matar.gueye@kiwi.sn>
2016-03-11 02:38:01 +0000

    Merge origin/master

--- backend/src/be/StockReel.php
+++ backend/src/be/StockReel.php
@@ -12,7 +12,7 @@ class StockReel {
     protected $id;
     
     /**
-     * @Column(type="decimal", scale=2, precision=10, nullable=true)
+     * @Column(type="decimal", scale=2, precision=10, nullable=true, options={"unsigned":true})
      * */
     protected $stock;
     /**
